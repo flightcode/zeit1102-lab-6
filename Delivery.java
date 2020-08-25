51cd1ee521883ec234aa8aafe8e462424c71bb04
Elliot Parker <elliotccp@protonmail.com>
2020-08-25 10:37:05 +1000

    Add task 2 content

--- Delivery.java
+++ Delivery.java
@@ -1,4 +1,21 @@
 public class Delivery {
     boolean[][] arrDelivered = new boolean[10][4];
     double[] arrPrices = new double[4];
+
+    public static void main(String[] args) {
+        System.out.println(new Delivery().toString());
+    }
+
+    public String toString() {
+        String output = "";
+
+        for (int i = 0; i < arrDelivered.length; i++) {
+            for (int j = 0; j < arrDelivered[i].length; j++) {
+                output += arrDelivered[i][j] + " ";
+            }
+            output += "; ";
+        }
+
+        return output;
+    }
 }
